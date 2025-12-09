ba040b38d4a07871617535976d39a30645e1bfed
Suhail-22 <101893675+Suhail-22@users.noreply.github.com>
2025-12-09 19:21:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,21 @@
-// Service Worker مبسط
+// Service Worker بسيط
 const CACHE_NAME = 'ai-hub-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
   './manifest.json'
 ];
 
-// Install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
